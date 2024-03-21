56673b9d1e904110ae07763032135a284ac9c04a
naathanbrown <nathancjb@googlemail.com>
2024-03-21 11:11:18 +0000

    feat(cb2-11440): Dynamo is the worst thing ever

--- src/dynamo/getDynamoRecords.ts
+++ src/dynamo/getDynamoRecords.ts
@@ -9,10 +9,13 @@ export const getDynamoMembers: () => Promise<IDynamoRecord[]> = async () => {
     .query({
       TableName: config.aws.dynamoTable,
       KeyConditionExpression: 'resourceType = :type',
-      FilterExpression: 'attribute_not_exists(ttl) or ttl = :null',
+      FilterExpression: 'attribute_not_exists(#ttl_key) or #ttl_key = :null',
       ExpressionAttributeValues: {
         ':type': ResourceType.User,
         ':null': null,
+      },
+      ExpressionAttributeNames: {
+        '#ttl_key': 'ttl',
       },
     } as AWS.DynamoDB.DocumentClient.QueryInput)
     .promise();
