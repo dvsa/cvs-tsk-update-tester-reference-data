1f822e3be617a6e5b8061081d84a3e6cfef350be
Michael <michael.mullen@dvsa.gov.uk>
2024-10-11 15:14:36 +0100

    feat(CB2-14232): update doc-gen-upload to remove snowball SQS pattern

--- src/handler.ts
+++ src/handler.ts
@@ -1,4 +1,4 @@
-import { Handler, SQSBatchResponse, SQSEvent } from 'aws-lambda';
+import {Handler, SQSBatchItemFailure, SQSBatchResponse, SQSEvent} from 'aws-lambda';
 import { TextDecoder } from 'util';
 import { DocumentModel } from './models/documentModel';
 import { getDocumentFromRequest } from './models/documentModel.factory';
@@ -13,50 +13,44 @@ export const handler: Handler = async (event: SQSEvent): Promise<SQSBatchRespons
     logger.error('ERROR: event is not defined.');
     throw new Error('Event is empty');
   }
-
-  const altPromiseArray = event.Records.map((sqsRecord) => {
-    const request = JSON.parse(sqsRecord.body) as Request;
-
-    const document: DocumentModel = getDocumentFromRequest(request);
-    return generateAndUpload(document, request);
-  });
-
-  const results = await Promise.allSettled(altPromiseArray);
-  const ids = results
-    .map((result, index) => (result.status === 'fulfilled' ? null : event.Records[index].messageId))
-    .filter((item) => item !== null);
-  return {
-    batchItemFailures: ids.map((id) => ({ itemIdentifier: id })),
-  };
+  const batchItemFailures: SQSBatchItemFailure[] = [];
+  // eslint-disable-next-line no-restricted-syntax
+  for (const sqsRecord of event.Records) {
+    try {
+      const request = JSON.parse(sqsRecord.body) as Request;
+      const document: DocumentModel = getDocumentFromRequest(request);
+      // eslint-disable-next-line no-await-in-loop
+      await generateAndUpload(document, request);
+    } catch (error) {
+      console.error(error);
+      batchItemFailures.push({ itemIdentifier: sqsRecord.messageId });
+    }
+  }
+  return { batchItemFailures };
 };
 
 const generateAndUpload = async (document: DocumentModel, request: Request) => {
-  try {
-    logger.info(`Starting lambda to lambda invoke (data): ${JSON.stringify(document)}`);
-    const response = await invokePdfGenLambda(document, request.documentName);
-    logger.info('Finished lambda to lambda invoke, checking response');
+  logger.info(`Starting lambda to lambda invoke (data): ${JSON.stringify(document)}`);
+  const response = await invokePdfGenLambda(document, request.documentName);
+  logger.info('Finished lambda to lambda invoke, checking response');
 
-    if (response.StatusCode !== 200) {
-      logger.error(`Error invoking doc gen (lambda call failed with ${response.StatusCode}`);
-      throw new Error(`Error invoking doc gen (lambda call failed with ${response.StatusCode}`);
-    }
-    const responseString: string = new TextDecoder().decode(response.Payload);
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const responseJson: any = JSON.parse(responseString);
-    if (responseJson.statusCode !== 200) {
-      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-      throw new Error(`Error returned from doc gen (${responseJson.statusCode}): ${responseJson.body}`);
-    }
-
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    const responseBuffer: Buffer = Buffer.from(responseJson.body, 'base64');
-    document.setFileSize(responseBuffer.byteLength);
-
-    logger.info(`Starting s3 upload for file: ${process.env.BRANCH}/${document.filename}`);
-    await uploadPdfToS3(responseBuffer, document.metaData, document.filename);
-    logger.info('Finished s3 upload');
-  } catch (error) {
-    logger.error(error.message);
-    throw error;
+  if (response.StatusCode !== 200) {
+    logger.error(`Error invoking doc gen (lambda call failed with ${response.StatusCode}`);
+    throw new Error(`Error invoking doc gen (lambda call failed with ${response.StatusCode}`);
   }
+  const responseString: string = new TextDecoder().decode(response.Payload);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const responseJson: any = JSON.parse(responseString);
+  if (responseJson.statusCode !== 200) {
+    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+    throw new Error(`Error returned from doc gen (${responseJson.statusCode}): ${responseJson.body}`);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  const responseBuffer: Buffer = Buffer.from(responseJson.body, 'base64');
+  document.setFileSize(responseBuffer.byteLength);
+
+  logger.info(`Starting s3 upload for file: ${process.env.BRANCH}/${document.filename}`);
+  await uploadPdfToS3(responseBuffer, document.metaData, document.filename);
+  logger.info('Finished s3 upload');
 };
